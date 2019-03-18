39efcf0459348b3c63221d2a7fdcec43eaddc4d6
georgehajjar <georgehajjar99@gmail.com>
2019-03-18 12:59:12 -0400

    Pushing to save

--- STATS.c
+++ STATS.c
@@ -1,113 +1,162 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <sys/shm.h>
+
+//#include "shm.h"
 
 // initialize test data 1
 int dataOne[5] = {5, 6, 8, 2, 7};
 // initialize test data 2
 int dataTwo[5] = {10, 9, 11, 5, 7};
 
-static int set_semvalue(void);
-static void del_semvalue(void);
-static int semaphore_p(void);
-static int semaphore_v(void);
+struct shared {
+  // initialize empty array to hold input data
+  int inputData[5];
+  //initialize array to hold semaphore id's
+  int sem_id[5];
+  //initialize an array of booolean with mutex's
+  bool isGreater[5];
+};
 
-static int sem_id;
+/* The function set_semvalue initializes the semaphore using the SETVAL command in a
+semctl call. We need to do this before we can use the semaphore. */
+static int set_semvalue(int i) {
+  union semun sem_union;
+  sem_union.val = 1;
+  if (semctl(shared->sem_id[i-1], 0, SETVAL, sem_union) == -1) {
+    return (0);
+  }
+  return (1);
+}
+
+/* The del_semvalue function has almost the same form, except the call to semctl uses
+the command IPC_RMID to remove the semaphore's ID. */
+static void del_semvalue(int i) {
+  union semun sem_union;
+  if (semctl(shared->sem_id[i-1], 0, IPC_RMID, sem_union) == -1) {
+    fprintf(stderr, "Failed to delete semaphore\n");
+  }
+}
+
+/* semaphore_p changes the semaphore by -1 (waiting). */
+static int semaphore_p(int i) {
+  struct sembuf sem_b;
+  sem_b.sem_num = 0;
+  sem_b.sem_op = -1; /* P() */
+  sem_b.sem_flg = SEM_UNDO;
+  if (semop(shared->sem_id[i-1], &sem_b, 1) == -1) {
+    fprintf(stderr, "semaphore_p failed\n");
+    return (0);
+  }
+  return (1);
+}
+
+/* semaphore_v is similar except for setting the sem_op part of the sembuf structure to 1,
+so that the semaphore becomes available. */
+static int semaphore_v(int i) {
+  struct sembuf sem_b;
+  sem_b.sem_num = 0;
+  sem_b.sem_op = 1; /* V() */
+  sem_b.sem_flg = SEM_UNDO;
+  if (semop(shared->sem_id[i-1], &sem_b, 1) == -1) {
+    fprintf(stderr, "semaphore_v failed\n");
+    return (0);
+  }
+  return (1);
+}
+
+
+void createProcesses(void) {
+  int i, tmp;
+  pid_t pid[4];
+
+  for(i = 0; i < 4; i++) {
+    pid[i] = fork();
+    if(pid[i] == 0) {
+      break;
+    }
+  }
+
+  if (pid[0] != 0 && pid[1] != 0 && pid[2] != 0 && pid[3] != 0) {
+    printf("I'm the parent process [pid: %d, ppid: %d]\n",getpid(),getppid());
+    for(i = 0; i < 4; i++) {
+      wait(&tmp);
+    }
+  } else {
+    printf("I'm a child process [pid: %d, ppid: %d]\n",getpid(),getppid());
+  }
+
+  for(i = 0; i < 4; i++) {
+    shared->sem_id[i]; = semget((key_t)1234+i, 1, 0666 | IPC_CREAT);
+
+    if (!set_semvalue(i)) {
+      fprintf(stderr, "Failed to initialize semaphore\n");
+			exit(EXIT_FAILURE);
+    }
+  }
+}
 
 int main(int argc, char* argv[]) {
-	int i;
-	int pause_time;
-	char op_char = 'O';
-	srand((unsigned int)getpid());
-	sem_id = semget((key_t)1234, 1, 0666 | IPC_CREAT);
 
-	if (argc > 1) {
-		if (!set_semvalue()) {
-			fprintf(stderr, "Failed to initialize semaphore\n");
-			exit(EXIT_FAILURE);
-		}
+  int running = 1;
+  void *shared_memory = (void *)0;
+  struct shared *shared_data;
+  int shmid;
 
-		op_char = 'X';
-		sleep(2);
-	}
+  shmid = shmget((key_t)1234, sizeof(struct shared), 0666 | IPC_CREAT);
 
-	for (i = 0; i < 10; i++) {
-		if (!semaphore_p()) {
-			exit(EXIT_FAILURE);
-		}
+  if (shmid == -1) {
+    fprintf(stderr, "shmget failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-		printf("%c", op_char);
-		fflush(stdout);
-		pause_time = rand() % 3;
-		sleep(pause_time);
-		printf("%c", op_char);
-		fflush(stdout);
+  shared_memory = shmat(shmid, (void *)0, 0);
+  if (shared_memory == (void *)-1) {
+    fprintf(stderr, "shmat failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-		if (!semaphore_v()) {
-			exit(EXIT_FAILURE);
-		}
+  printf("Memory attached at %X\n", (int)shared_memory);
 
-		pause_time = rand() % 2;
-		sleep(pause_time);
-	}
+  shared_data = (struct shared *)shared_memory;
+  for(int i = 0; i < 5; i++) {
+    shared_data->isGreater[i] = false;
+  }
 
-	printf("\n%d - finished\n", getpid());
+  printf("Enter 5 distinct integers to be sorted (seperated by commas).\n");
+  scanf("%d,%d,%d,%d,%d", &shared_data->inputData[0], &shared_data->inputData[1], &shared_data->inputData[2], &shared_data->inputData[3], &shared_data->inputData[4]);
 
-	if (argc > 1) {
-		sleep(10);
-		del_semvalue();
-	}
+  for(int i = 0; i < 5; i++) {
+    printf("%d\n", shared_data->inputData[i]);
+  }
 
-	exit(EXIT_SUCCESS);
-}
+  // while(running) {
+  //   if (shared_stuff->written_by_you) {
+  //     printf("You wrote: %s", shared_stuff->some_text);
+  //     sleep(rand() % 4); /*Make the other process wait for us!*/
+  //     shared_stuff->written_by_you = 0;
+  //     if (strncmp(shared_stuff->some_text, "end", 3) == 0) {
+  //       running = 0;
+  //     }
+  //   }
+  // }
 
-static int set_semvalue(void) {
-	union semun sem_union;
-	sem_union.val = 1;
+  if (shmdt(shared_memory) == -1) {
+    fprintf(stderr, "shmdt failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-	if (semctl(sem_id, 0, SETVAL, sem_union) == -1) {
-		return (0);
-	}
+  if (shmctl(shmid, IPC_RMID, 0) == -1) {
+    fprintf(stderr, "shmctl(IPC_RMID) failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-	return (1);
-}
-
-static void del_semvalue(void) {
-	union semun sem_union;
-
-	if (semctl(sem_id, 0, IPC_RMID, sem_union) == -1) {
-		fprintf(stderr, "Failed to delete semaphore\n");
-	}
-}
-
-static int semaphore_p(void) {
-	struct sembuf sem_b;
-	sem_b.sem_num = 0;
-	sem_b.sem_op = -1; /* P() */
-	sem_b.sem_flg = SEM_UNDO;
-
-	if (semop(sem_id, &sem_b, 1) == -1) {
-		fprintf(stderr, "semaphore_p failed\n");
-		return (0);
-	}
-
-	return (1);
-}
-
-static int semaphore_v(void) {
-	struct sembuf sem_b;
-	sem_b.sem_num = 0;
-	sem_b.sem_op = 1; /* V() */
-	sem_b.sem_flg = SEM_UNDO;
-
-	if (semop(sem_id, &sem_b, 1) == -1) {
-		fprintf(stderr, "semaphore_v failed\n");
-		return (0);
-	}
-
-	return (1);
+  createProcesses();
 }
---
+++ shm.h
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+
+struct shared {
+  // initialize empty array to hold input data
+  int inputData[5];
+  //initialize an array of booolean with mutex's
+  bool isGreater[5];
+};
