572eea0c595a08d96d8267c2cebed310082598d1
georgehajjar <georgehajjar99@gmail.com>
2019-03-18 20:22:22 -0400

    Close to finish, one error remaining

--- STATS.c
+++ STATS.c
@@ -9,8 +9,6 @@
 #include <sys/sem.h>
 #include <sys/shm.h>
 
-//#include "shm.h"
-
 // initialize test data 1
 int dataOne[5] = {5, 6, 8, 2, 7};
 // initialize test data 2
@@ -19,18 +17,22 @@ int dataTwo[5] = {10, 9, 11, 5, 7};
 struct shared {
   // initialize empty array to hold input data
   int inputData[5];
-  //initialize array to hold semaphore id's
-  int sem_id[5];
-  //initialize an array of booolean with mutex's
-  bool isGreater[5];
 };
+
+static int set_semvalue(int i);
+static void del_semvalue(int i);
+static int semaphore_p(int i);
+static int semaphore_v(int i);
+
+//initialize array to hold semaphore id's
+int sem_id[5];
 
 /* The function set_semvalue initializes the semaphore using the SETVAL command in a
 semctl call. We need to do this before we can use the semaphore. */
 static int set_semvalue(int i) {
   union semun sem_union;
   sem_union.val = 1;
-  if (semctl(shared->sem_id[i-1], 0, SETVAL, sem_union) == -1) {
+  if (semctl(sem_id[i], 0, SETVAL, sem_union) == -1) {
     return (0);
   }
   return (1);
@@ -40,7 +42,7 @@ static int set_semvalue(int i) {
 the command IPC_RMID to remove the semaphore's ID. */
 static void del_semvalue(int i) {
   union semun sem_union;
-  if (semctl(shared->sem_id[i-1], 0, IPC_RMID, sem_union) == -1) {
+  if (semctl(sem_id[i], 0, IPC_RMID, sem_union) == -1) {
     fprintf(stderr, "Failed to delete semaphore\n");
   }
 }
@@ -51,7 +53,7 @@ static int semaphore_p(int i) {
   sem_b.sem_num = 0;
   sem_b.sem_op = -1; /* P() */
   sem_b.sem_flg = SEM_UNDO;
-  if (semop(shared->sem_id[i-1], &sem_b, 1) == -1) {
+  if (semop(sem_id[i], &sem_b, 1) == -1) {
     fprintf(stderr, "semaphore_p failed\n");
     return (0);
   }
@@ -65,41 +67,41 @@ static int semaphore_v(int i) {
   sem_b.sem_num = 0;
   sem_b.sem_op = 1; /* V() */
   sem_b.sem_flg = SEM_UNDO;
-  if (semop(shared->sem_id[i-1], &sem_b, 1) == -1) {
+  if (semop(sem_id[i], &sem_b, 1) == -1) {
     fprintf(stderr, "semaphore_v failed\n");
     return (0);
   }
   return (1);
 }
 
+bool debug(void) {
+  char answer;
 
-void createProcesses(void) {
-  int i, tmp;
-  pid_t pid[4];
+  printf("Do you want to use this program in debug mode? (y/n)\n");
+  scanf(" %c", &answer);
 
-  for(i = 0; i < 4; i++) {
-    pid[i] = fork();
-    if(pid[i] == 0) {
-      break;
-    }
+  if (strcmp(&answer, "y") == 0) {
+    return true;
   }
-
-  if (pid[0] != 0 && pid[1] != 0 && pid[2] != 0 && pid[3] != 0) {
-    printf("I'm the parent process [pid: %d, ppid: %d]\n",getpid(),getppid());
-    for(i = 0; i < 4; i++) {
-      wait(&tmp);
-    }
-  } else {
-    printf("I'm a child process [pid: %d, ppid: %d]\n",getpid(),getppid());
+  else {
+    return false;
   }
+}
 
-  for(i = 0; i < 4; i++) {
-    shared->sem_id[i]; = semget((key_t)1234+i, 1, 0666 | IPC_CREAT);
+void createSemaphores(void) {
+  for(int i = 0; i < 4; i++) {
+    sem_id[i] = semget((key_t)1234+i, 1, 0666 | IPC_CREAT);
 
     if (!set_semvalue(i)) {
       fprintf(stderr, "Failed to initialize semaphore\n");
-			exit(EXIT_FAILURE);
+      exit(EXIT_FAILURE);
     }
+  }
+}
+
+void destorySemaphores(void) {
+  for(int i = 0; i < 4; i++) {
+    del_semvalue(i);
   }
 }
 
@@ -109,6 +111,7 @@ int main(int argc, char* argv[]) {
   void *shared_memory = (void *)0;
   struct shared *shared_data;
   int shmid;
+  bool debugOn = debug();
 
   shmid = shmget((key_t)1234, sizeof(struct shared), 0666 | IPC_CREAT);
 
@@ -123,12 +126,9 @@ int main(int argc, char* argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  printf("Memory attached at %X\n", (int)shared_memory);
+  //printf("Memory attached at %X\n", (int)shared_memory);
 
   shared_data = (struct shared *)shared_memory;
-  for(int i = 0; i < 5; i++) {
-    shared_data->isGreater[i] = false;
-  }
 
   printf("Enter 5 distinct integers to be sorted (seperated by commas).\n");
   scanf("%d,%d,%d,%d,%d", &shared_data->inputData[0], &shared_data->inputData[1], &shared_data->inputData[2], &shared_data->inputData[3], &shared_data->inputData[4]);
@@ -137,26 +137,166 @@ int main(int argc, char* argv[]) {
     printf("%d\n", shared_data->inputData[i]);
   }
 
-  // while(running) {
-  //   if (shared_stuff->written_by_you) {
-  //     printf("You wrote: %s", shared_stuff->some_text);
-  //     sleep(rand() % 4); /*Make the other process wait for us!*/
-  //     shared_stuff->written_by_you = 0;
-  //     if (strncmp(shared_stuff->some_text, "end", 3) == 0) {
-  //       running = 0;
-  //     }
-  //   }
-  // }
+  createSemaphores();
 
-  if (shmdt(shared_memory) == -1) {
-    fprintf(stderr, "shmdt failed\n");
-    exit(EXIT_FAILURE);
+  int time, tmp;
+  pid_t pid[4];
+
+  for(int i = 0; i < 4; i++) {
+    pid[i] = fork();
+    if (pid[i] == 0) {
+      break;
+    }
   }
 
-  if (shmctl(shmid, IPC_RMID, 0) == -1) {
-    fprintf(stderr, "shmctl(IPC_RMID) failed\n");
-    exit(EXIT_FAILURE);
+  if (pid[0] != 0 && pid[1] != 0 && pid[2] != 0 && pid[3] != 0) {
+    //printf("I'm the parent process [pid: %d, ppid: %d]\n",getpid(),getppid());
+    for(int i = 0; i < 4; i++) {
+      wait(&time);
+    }
   }
+  else {
+    printf("I'm a child process [pid: %d, ppid: %d]\n",getpid(),getppid());
 
-  createProcesses();
-}
+    while (!(
+      shared_data->inputData[0] >= shared_data->inputData[1] &&
+      shared_data->inputData[1] >= shared_data->inputData[2] &&
+      shared_data->inputData[2] >= shared_data->inputData[3] &&
+      shared_data->inputData[3] >= shared_data->inputData[4])) {
+
+        if (pid[0] == 0) {
+          if (!semaphore_p(0)) {
+            exit(EXIT_FAILURE);
+          }
+
+          if (shared_data->inputData[0] < shared_data->inputData[1]) {
+            if(debugOn) {
+              printf("Process %d: performed swapping\n", getpid());
+            }
+            tmp = shared_data->inputData[0];
+            shared_data->inputData[0] = shared_data->inputData[1];
+            shared_data->inputData[1] = tmp;
+          }
+          else {
+            if(debugOn) {
+              printf("Process %d: No swapping\n", getpid());
+            }
+          }
+
+          if (!semaphore_v(0)) {
+            exit(EXIT_FAILURE);
+          }
+        }
+
+        else if (pid[1] == 0) {
+          if (!semaphore_p(0)) {
+            exit(EXIT_FAILURE);
+          }
+          if (!semaphore_p(1)) {
+            exit(EXIT_FAILURE);
+          }
+
+          if (shared_data->inputData[1] < shared_data->inputData[2]) {
+            if(debugOn) {
+              printf("Process %d: performed swapping\n", getpid());
+            }
+            tmp = shared_data->inputData[1];
+            shared_data->inputData[1] = shared_data->inputData[2];
+            shared_data->inputData[2] = tmp;
+          }
+          else {
+            if(debugOn) {
+              printf("Process %d: No swapping\n", getpid());
+            }
+          }
+
+          if (!semaphore_v(0)) {
+            exit(EXIT_FAILURE);
+          }
+          if (!semaphore_v(1)) {
+            exit(EXIT_FAILURE);
+          }
+        }
+
+        else if (pid[2] == 0) {
+          if (!semaphore_p(1)) {
+            exit(EXIT_FAILURE);
+          }
+          if (!semaphore_p(2)) {
+            exit(EXIT_FAILURE);
+          }
+
+          if (shared_data->inputData[1] < shared_data->inputData[2]) {
+            if(debugOn) {
+              printf("Process %d: performed swapping\n", getpid());
+            }
+            tmp = shared_data->inputData[1];
+            shared_data->inputData[1] = shared_data->inputData[2];
+            shared_data->inputData[2] = tmp;
+          }
+          else {
+            if(debugOn) {
+              printf("Process %d: No swapping\n", getpid());
+            }
+          }
+
+          if (!semaphore_v(1)) {
+            exit(EXIT_FAILURE);
+          }
+          if (!semaphore_v(2)) {
+            exit(EXIT_FAILURE);
+          }
+        }
+
+        else if (pid[3] == 0) {
+          if (!semaphore_p(2)) {
+            exit(EXIT_FAILURE);
+          }
+          if (!semaphore_p(3)) {
+            exit(EXIT_FAILURE);
+          }
+
+          if (shared_data->inputData[3] < shared_data->inputData[4]) {
+            if(debugOn) {
+              printf("Process %d: performed swapping\n", getpid());
+            }
+            tmp = shared_data->inputData[3];
+            shared_data->inputData[3] = shared_data->inputData[4];
+            shared_data->inputData[4] = tmp;
+          }
+          else {
+            if(debugOn) {
+              printf("Process %d: No swapping\n", getpid());
+            }
+          }
+
+          if (!semaphore_v(2)) {
+            exit(EXIT_FAILURE);
+          }
+          if (!semaphore_v(3)) {
+            exit(EXIT_FAILURE);
+          }
+        }
+      }
+    }
+
+    printf("Sorted list:");
+    for(int i = 0; i < 5; i++) {
+      printf("%d ", shared_data->inputData[i]);
+    }
+    printf("\nMinimum value: %d", shared_data->inputData[0]);
+    printf("\nMaximum value: %d", shared_data->inputData[4]);
+    printf("\nMedian value: %d", shared_data->inputData[2]);
+
+    destorySemaphores();
+
+    if (shmdt(shared_memory) == -1) {
+      fprintf(stderr, "shmdt failed\n");
+      exit(EXIT_FAILURE);
+    }
+
+    if (shmctl(shmid, IPC_RMID, 0) == -1) {
+      fprintf(stderr, "shmctl(IPC_RMID) failed\n");
+      exit(EXIT_FAILURE);
+    }
+  }
--- shm.h
+++
@@ -1,8 +0,0 @@
-#include <stdbool.h>
-
-struct shared {
-  // initialize empty array to hold input data
-  int inputData[5];
-  //initialize an array of booolean with mutex's
-  bool isGreater[5];
-};
